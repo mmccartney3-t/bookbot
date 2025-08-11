edbbe61d9ececa86c21b1718f2a06830cb8b2aff
mmccartney3-t <michaelmccartney123@hotmail.com>
2025-08-11 20:55:59 +0100

    Fixed issue with input. Added word count

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import sys
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
         return file_contents
+def num_of_words(file_contents):
+    words = file_contents.split()
+    word_count = len(words)
+    return word_count
 
 def main():
-    file_path = input("What is the file path\n")
+    file_path = "books/frankenstein.txt"
     contents = get_book_text(file_path)
-    print(contents)
+    word_count = num_of_words(contents)
+    print(f"{word_count} words found in the document")
+
 
 main()
